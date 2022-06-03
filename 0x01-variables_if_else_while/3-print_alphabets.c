4176f311b67a4b3b3f2e3b2cdfb3803c86726473
Esther Muiga <esthermuiga3@gmail.com>
2022-06-03 09:58:48 -0700

    Lowercase and uppercase letters following each other

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints lowercase and upperase letters
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}

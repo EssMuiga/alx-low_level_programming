e96cb92e1a7d13b647749cea1e2d46502740195c
Esther Muiga <esthermuiga3@gmail.com>
2022-06-03 08:07:38 -0700

    Alphabets

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,27 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
+	int last_digit;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	int n, lastDigit;
-	lastDigit = n % 10;
+	last_digit = n%10;
 	if (n > 5)
 	{
-		printf("Last digit of n is %d and is greater than 5\n");
+		printf("Last digit of %d is n and is greater than 5\n");
 	}
-	if (n == 0)
+	else if (n == 0)
 	{
-		printf("Last digit of n is %d and is zero\n")
+		printf("Last digit of %d is n and is 0\n");
 	}
 	else
 	{
-		printf("Last digit of n is %d and is less than 6 and not 0\n")
+		printf("Last digit of %d is n and is less than 6 and not 0\n");
 	}
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/* working on loe lwvwl programing */
+int main(void)
+{
+	char alphabets;
+	for (alphabets = a; alphabets <=z; alphabets ++)
+	{
+		putchar(alphabets);
+	}
+	putchar("\n");
+	return (0);
+}

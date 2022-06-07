442d9aa794485f94f725b8ec54510a1c814df230
EssMuiga <104901226+EssMuiga@users.noreply.github.com>
2022-06-07 18:05:31 +0300

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,16 +1,17 @@
-#include <stdio.h>
-/**
- * main - print alphabets z to a
- * Return: 0 as successful
+/*
+ * File: 0-putchar.c
  */
+#include "main.h"
 int main(void)
 {
-	char c;
-
-	for (c = 'z'; c >= 'a'; c--)
-	{
-		putchar(c);
-	}
-	putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }

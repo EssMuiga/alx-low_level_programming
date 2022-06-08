4adecb029a8efaadccb29db1d8b6fbf4e8db632e
Esther Muiga <esthermuiga3@gmail.com>
2022-06-07 23:39:37 -0700

    Updating putchar file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
-<<<<<<< HEAD
-#include "main.h"
-=======
-#include <unistd.h>
-#include <main.h>
->>>>>>> 9f47ae2c13567598d985be338da364ce2493ec00
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - check code
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * return: 0 if successful
  */
-int _putchar(char c)
+int main(void)
 {
-	int main(void)
-{
-	printf("_putchar\n");
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 	return (0);
-}
-	return (write(1, &c, 1));
 }

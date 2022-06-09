cd6c75d961c8524f351b08a32180617cd9eea542
Esther Muiga <esthermuiga3@gmail.com>
2022-06-09 04:32:19 -0700

    Printing numbers using putchar

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * print_numbers - function that prints numbers from 0 to 9
+ */
+void print_numbers(void)
+{
+
+	int num;
+	for (num = 0; num <= 9; num++)
+	{
+		putchar((num % 10) + '0');
+	}
+	putchar('\n');
+}

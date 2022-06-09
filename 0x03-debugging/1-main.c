8ca85a1b97361a0bc80f86401cd4d823237773c2
Esther Muiga <esthermuiga3@gmail.com>
2022-06-09 08:29:58 -0700

    Printing largest integer

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,8 +12,7 @@ int main(void)
 	printf("Infinite loop incoming : (\n");
 
 	i = 0;
-	
-	while (i < 10) /*this line of code causes theinfinite loop as i will always be less than 10*/
+	while (i < 10)
 	{
 		putchar(i);
 	}
--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -8,22 +8,19 @@
  * return: largest number
  */
 
-int largest_number( int a, int b, int c)
+int largest_number(int a, int b, int c)
 {
-	int largest;
-
 	if (a > b && b > c)
 	{
-		largest = a;
+		printf("%d", a);
 	}
 	else if (b > a && a > c)
 	{
-		largest = b;
+		printf("%d", b);
 	}
 	else
 	{
-		largest = c;
+		printf("%d", c);
 	}
-
-	return (largest);
+	return (0);
 }

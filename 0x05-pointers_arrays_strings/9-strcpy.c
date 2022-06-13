a95fe1b597e634ddba78841cdc5746dbc007c618
Esther Muiga <esthermuiga3@gmail.com>
2022-06-13 10:19:35 -0700

    Copying a string into another string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * char *_strcpy - function that copies string pointed to the src
+ * @dest: the one that's being copied to
+ * @src: the one it is being copied from
+ * return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	for (; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}

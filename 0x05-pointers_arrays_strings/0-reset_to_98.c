c4a5c9a98e800bb5a2b1ac4bf6ea12a694af639a
Esther Muiga <esthermuiga3@gmail.com>
2022-06-13 05:09:58 -0700

    Taking a pointer to an integer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * reset_to_98 is a function that brings the pointer to an integer
+ * @n: the integer in question
+ * return: 0 if successful
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+This project looks at pointers, arrays and strings

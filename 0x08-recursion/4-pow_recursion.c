b37000e765f4c6115e9c0da800b388c377c62acc
Esther Muiga <esthermuiga3@gmail.com>
2022-06-22 02:17:43 -0700

    Fuction that gets a number raised to another

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * _pow_recursion - Function that returns x raised to y
+ * @ x: integer
+ * @y: integer
+ * Return: 0 if suucessful
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}

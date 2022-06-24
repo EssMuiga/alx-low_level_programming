0b34fe21d0263e8afee7cead0e6809019a635051
Esther Muiga <esthermuiga3@gmail.com>
2022-06-24 01:41:12 -0700

    Program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Function that prints its own name
+ * @argc: the arguments count
+ * @argv: the array pointing to the arguments
+ * Return: 0 if successful
+ */
+int main(int argc, char **argv)
+{
+	argc = 0;
+
+	printf("%s\n", argv[argc]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+This project is dealing with argc and argv

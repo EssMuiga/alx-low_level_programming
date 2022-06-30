3d50fe58085713d60342efefd1ad5fb2d0808751
EssMuiga <104901226+EssMuiga@users.noreply.github.com>
2022-06-30 22:55:33 +0300

    Update 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -13,17 +13,12 @@ int *array_range(int min, int max)
 	int i, len;
 
 	if (min > max)
-	{
 		return (NULL);
-	}
 	for (len = 0; len < (max - min); len++)
-	{
-		arr = malloc(sizeof(int) * (len + 1));
-	}
+		;
+	arr = malloc(sizeof(int) * (len + 1));
 	if (arr == NULL)
-	{
 		return (NULL);
-	}
 	for (i = 0; i <= len; i++)
 	{
 		arr[i] = min++;

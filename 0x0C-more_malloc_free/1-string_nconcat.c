0a52ac830b90796005ea38b7dbeeb52f25849734
EssMuiga <104901226+EssMuiga@users.noreply.github.com>
2022-06-30 22:06:29 +0300

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,39 +9,28 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *arr;
-	unsigned int a, b, co, co_2;
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	if (s1 == NULL)
-	{
-		s1 = "";
-	}
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
-	for (a = 0; s1[a] != '\0'; a++)
-	{
-	}
-	for (b = 0; s2[b] != '\0'; b++)
-	{
-	}
-	if (n < b)
-	{
-		b = n;
-	}
-	b += a;
-	arr = malloc(sizeof(char *) * (b + 1));
-	if (arr == NULL)
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (!s)
 		return (NULL);
-	for (co = 0; co < a; co++)
-		arr[co] = s1[co];
-	for (co_2 = 0; co < b; co_2++)
+	while (i < len1)
 	{
-		arr[co] = s2[co_2];
-		co++;
+		s[i] = s1[i];
+		i++;
 	}
-	co++;
-	arr[co] = '\0';
-	return (arr);
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
+	s[i] = '\0';
+	return (s);
 }

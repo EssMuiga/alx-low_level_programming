d0cc21aee8637722e1fed09b414903faa8b4e66c
Esther Muiga <esthermuiga@gmail.com>
2022-07-06 10:21:10 -0700

    Functions in file 3 ops

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * op_add - Function that gets the sum of a and b
+ * @a: integer
+ * @b: integer
+ * Return: 0
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - Function to get the subtraction
+ * @a: integer
+ * @b: integer
+ * Return: 0
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - Function for multiplication
+ * @a: integer
+ * @b: integer
+ * Return: 0
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - Function for division
+ * @a: integer
+ * @b: integer
+ * Return: 0
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - Function for modulus
+ * @a: integer
+ * @b: integer
+ * Return: 0
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
